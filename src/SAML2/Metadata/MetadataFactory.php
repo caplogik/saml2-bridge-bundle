cbdc97994d12e4e42e74639e2c91cc843ed00ab9
wryk <wryk@wryk.moe>
2025-07-17 16:39:54 +0200

    forgot one issue

--- src/SAML2/Metadata/MetadataFactory.php
+++ src/SAML2/Metadata/MetadataFactory.php
@@ -58,7 +58,7 @@ class MetadataFactory
         $response = new Response();
 
         $response->setContent($this->twig->render(
-            "AdactiveSasSaml2BridgeBundle:Metadata:metadata.xml.twig",
+            "@AdactiveSasSaml2Bridge/Metadata/metadata.xml.twig",
             [
                 "metadata" => $this->buildMetadata()
             ]
